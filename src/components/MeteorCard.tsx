d21ea25bae691a4c2c89215bff04126b12a5d2ef
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-04-13 22:34:04 +0530

    home card changes

--- src/components/MeteorCard.tsx
+++ src/components/MeteorCard.tsx
@@ -10,7 +10,7 @@ export function MeteorCard() {
       <div className="relative w-[94vw] md:w-full max-w-xl max-h-xl">
         <div className="absolute inset-0 h-full w-full scale-[0.80] transform rounded-full bg-red-500 bg-gradient-to-r from-blue-500 to-teal-500 blur-3xl" />
         <div className="relative flex h-full flex-col items-start justify-end overflow-hidden rounded-2xl border border-gray-800 bg-gray-900 px-4 py-8 shadow-xl">
-          <div className=" hidden lg:flex mb-4 h-5 w-5 items-center justify-center rounded-full border border-gray-500">
+          <div className="flex mb-4 h-5 w-5 items-center justify-center rounded-full border border-gray-500">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
