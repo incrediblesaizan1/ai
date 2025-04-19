72365c93f64d91b5cc064a5b81c9899fcf76e68c
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-04-20 01:27:40 +0530

    minor ui changes

--- src/app/dashboard/[[...rest]]/page.tsx
+++ src/app/dashboard/[[...rest]]/page.tsx
@@ -133,7 +133,7 @@ const Page = () => {
                     className={cn(
                       "w-fit max-w-[100vw] md:max-w-[70vw] mb-6 lg:max-w-[52vw] p-2 mt-3 rounded-md mx-auto md:mx-0",
                       msg.type === "outgoing"
-                        ? "bg-[#060b25] self-start"
+                        ? "bg-[#060b25] self-start font-sans text-xl md:text-md"
                         : "bg-[#27272a] self-start"
                     )}
                   >
@@ -170,7 +170,7 @@ const Page = () => {
               value={message}
               placeholder="Ask anything"
               onChange={(e) => setMessage(e.target.value)}
-              className=" textinp bg-transparent resize-none text-justify rounded-xl p-2 w-[50vw] md:w-[60vw] lg:w-[50vw] outline-none custom-scrollbar2 min-h-20 md:min-h-28 max-h-96 overflow-y-auto "
+              className=" font-sans textinp bg-transparent resize-none text-justify rounded-xl p-2 w-[50vw] md:w-[60vw] lg:w-[50vw] outline-none custom-scrollbar2 min-h-20 md:min-h-28 max-h-96 overflow-y-auto "
               ref={(el) => {
                 if (el) {
                   el.style.height = "auto";
